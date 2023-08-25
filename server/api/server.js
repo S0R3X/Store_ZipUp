96643f70ad0db21c6e056b9f39384c6fedc13726
Volodya <unu-unu-unu@mail.ru>
2023-08-25 04:25:20 +0300

    trial deploy

---
+++ server/api/server.js
@@ -0,0 +1,22 @@
+// See https://github.com/typicode/json-server#module
+import { create, router as _router, defaults, rewriter } from "json-server";
+const server = create();
+const router = _router("db.json");
+const middlewares = defaults();
+
+server.use(middlewares);
+// Add this before server.use(router)
+server.use(
+  rewriter({
+    "/api/*": "/$1",
+    "/blog/:resource/:id/show": "/:resource/:id",
+  })
+);
+server.use(router);
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
+
+// Export the Server API
+// eslint-disable-next-line no-undef
+export default server;
