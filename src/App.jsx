f0e46202b52c301d08f83ae70b2063ce8cfb4cf8
Volodya <unu-unu-unu@mail.ru>
2023-08-25 18:48:06 +0300

    change struct of project

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 
 function App() {
-  fetch("http://localhost:3333/posts")
+  fetch("https://store-zip-up.vercel.app/comments")
     .then((response) => response.json())
     .then((commits) => console.log(commits));
 
